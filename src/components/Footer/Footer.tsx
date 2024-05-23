711ea38f224804685eb02698563b1f427f38b477
Mehrdad Mahmoodi <78253050+Mehrdad-Mh@users.noreply.github.com>
2024-05-23 20:28:38 +0330

    design footer and styled it

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,12 @@ import "./Footer.scss"
 export default function Footer() {
   return (
     <div className='footer'>
-      فووووتر
+     
+     <div className="created">
+      <img src="/mehrdad.png" alt="logo" />
+      <a href="https://github.com/Mehrdad-Mh">  <h3>  مهرداد محمودی  </h3>  </a>
+     </div>
+     <div className="copyRight">  <h5>  کلیه حقوق استفاده از محتوا مطعلق به این سایت است   </h5> </div>
     </div>
   )
 }
